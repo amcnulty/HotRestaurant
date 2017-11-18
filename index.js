4de846774dbfe14daa8007730252c6edf01b7958
yamalik95 <yamalik@ucsd.edu>
2017-11-18 11:55:57 -0800

    Set up page routes

--- index.js
+++ index.js
@@ -4,17 +4,19 @@ var path = require('path');
 var bodyParser = require('body-parser');
 require('dotenv').config();
 
-var pageRoutes = require('./routes/pageRoutes');
+var pageRoutes = require('./roots/pageroutes');
 
 var app = express();
-
-app.set('views', path.join(__dirname, 'views'));
 
 app.use(favicon(path.join(__dirname, 'public', 'res/icon/favicon.ico')));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname + '/public')));
 
 app.use('/', pageRoutes);
+
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/views/home.html')
+});
 
 app.set('port', (process.env.PORT || 5000));
 
