e826d18a952cee60fca78d2aba8ff035e59cf95b
Yan Kwong <yankwong@Yans-MacBook-Air.local>
2017-11-18 13:25:15 -0800

    error done

--- public/js/reserve.js
+++ public/js/reserve.js
@@ -3,18 +3,50 @@ var YTK = YTK || {};
 
 YTK.reserve = (function($) {
   var 
+  verifyInput = function(value, $class) {
+    if (typeof (value) === 'undefined' || value.trim() === '') {
+      showWarning($class);
+      return false;
+    }
+    else {
+      return true;
+    }
+  },
+  showWarning = function($div) {
+    $div.closest('.input-row').find('.warning').show('slow');
+  },
+  hideWarnings = function() {
+    $('.warning').hide();
+  },
   grabFormData = function() {
-    var $name   = $('.name', '.reserve-page'),
-        $phone  = $('.phone', '.reserve-page'),
-        $email  = $('.email', '.reserve-page'),
-        $uniqueID = $('.unique-id', '.reserve-page');
+    var $name   = $('input.name', '.reserve-page'),
+        $phone  = $('input.phone', '.reserve-page'),
+        $email  = $('input.email', '.reserve-page'),
+        $uniqueID = $('input.unique-id', '.reserve-page'),
+        name = $name.val(),
+        phone = $phone.val(),
+        email = $email.val(),
+        uid = $uniqueID.val(),
+        retObj = {},
+        resultArr = [];
 
-    return {
-      name  : $name.val() || '',
-      phone : $phone.val() || '',
-      email : $email.val() || '',
-      uid   : $uniqueID.val() || ''
-    };
+        //check all
+        resultArr.push(verifyInput(name, $name));
+        resultArr.push(verifyInput(phone, $phone));
+        resultArr.push(verifyInput(email, $email));
+        resultArr.push(verifyInput(uid, $uniqueID));
+
+
+    if (resultArr.indexOf(false) === -1) {
+      hideWarnings();
+      retObj = {
+        name  : $name.val() || '',
+        phone : $phone.val() || '',
+        email : $email.val() || '',
+        uid   : $uniqueID.val() || ''
+      };  
+    }
+    return retObj;
   },
   //TODO: add error checking
   setupSubmit = function() {
@@ -24,17 +56,20 @@ YTK.reserve = (function($) {
       e.preventDefault();
       var formObj = grabFormData();
 
-      // push to API
-      $.post("/api/new", formObj)
-      .done(function(data) {
-        console.log(data);
-        if (true) {
-          alert("Your Reservation is on there");
-        }
-        else {
-          alert("Your Reservation is on the waiting list");  
-        }
-      });
+      if (!$.isEmptyObject(formObj)) {
+        // push to API
+        $.post("/api/new", formObj)
+        .done(function(data) {
+          console.log(data);
+          if (true) {
+            alert("Your Reservation is on there");
+          }
+          else {
+            alert("Your Reservation is on the waiting list");  
+          }
+        });
+      }
+      
     });
   },
   initPage = function() {
